b59c338bac1c2dee6a1f99d8ea692ae8ec184173
ananta <anantalamichhane1@gmail.com>
2019-12-02 18:14:57 +0545

    color given fro congrts message

--- app/javascript/bundles/boggle/components/board.js.jsx
+++ app/javascript/bundles/boggle/components/board.js.jsx
@@ -22,7 +22,7 @@ export default class Board extends React.Component {
   handleTextChange(e, input_field) {
     if (e.key === 'Enter') {
       if (this.props.valid_words.indexOf(input_field.value) > -1) {
-        if (!(this.state.words.indexOf(input_field.value) > -1)){
+        if (!(this.state.words.indexOf(input_field.value) > -1)) {
           this.setState({words: this.state.words.concat(input_field.value)})
           this.setState({desc: 'Valid word'})
           this.setState({score: this.state.score + input_field.value.length})
@@ -34,13 +34,12 @@ export default class Board extends React.Component {
     }
   }
 
-
-  handleTimer(timer){
-    if(!timer)
+  handleTimer(timer) {
+    if (!timer)
       this.setState({timer: false})
   }
 
-  handleReloadClick(){
+  handleReloadClick() {
     window.location.reload();
   }
 
@@ -52,18 +51,18 @@ export default class Board extends React.Component {
     const isAllWords = this.state.words;
     let message;
 
-    if (isAllWords.length == this.props.valid_words.length && this.props.valid_words.length > 0 ){
-      message = <div>Congratulations.Reload page to play again.</div>
-    }else {
-      if (this.state.timer){
+    if (isAllWords.length == this.props.valid_words.length && this.props.valid_words.length > 0) {
+      message = <div style={{color: "green"}}><p>Congratulations. Reload page to play again.</p></div>
+    } else {
+      if (this.state.timer) {
         message = <div>
           <p>Please find {this.props.valid_words.length} words from board.</p>
-          <p style={{color:"green"}}> {this.state.desc} </p>
+          <p style={{color: "green"}}> {this.state.desc} </p>
         </div>
-      }else{
-        message = <div>Times Up. Reload page to play again. <p><b>Valid words:</b> {this.props.valid_words.join(",")} </p></div>
+      } else {
+        message = <div><p style={{color:"red"}}>Times Up. Reload page to play again.</p> <p><b>Valid
+          words:</b> {this.props.valid_words.join(",")} </p></div>
       }
-
     }
 
     return (
@@ -82,20 +81,22 @@ export default class Board extends React.Component {
       <table border="1">
         <tbody>
         <tr>
-          <td width="50%">
-            Board
+          <td width="50%" valign="top">
+            <b>Board</b>
             <table border="1">
               <tbody>
               <tr>
                 <td colSpan="4">
-                  <InputForm timer={this.state.timer} handleTextChange={this.handleTextChange.bind(this)}/>
+                  <InputForm timer={this.state.timer}
+                             handleTextChange={this.handleTextChange.bind(this)}/>
                 </td>
               </tr>
               {boardRows}
               </tbody>
             </table>
           </td>
-          <td width="50%">
+          <td width="50%" valign="top">
+            <b>List of valid inputs</b>
             <WordArea words={this.state.words}/>
           </td>
         </tr>
