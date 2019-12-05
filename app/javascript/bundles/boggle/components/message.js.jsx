d039703f039db49a237ae9a0652a3b4b8aeb6c4f
ananta <anantalamichhane1@gmail.com>
2019-12-05 23:02:43 +0545

    message jsx refactered.

--- app/javascript/bundles/boggle/components/message.js.jsx
+++ app/javascript/bundles/boggle/components/message.js.jsx
@@ -7,16 +7,14 @@ export default class Message extends React.Component {
 
     if (this.props.input_words.length == this.props.valid_words.length && this.props.valid_words.length > 0) {
       message = <div style={{color: "green"}}><p>Congratulations. Reload page to play again.</p></div>
-    } else {
-      if (this.props.timer) {
-        message = <div>
-          <p>Please find {this.props.valid_words.length} words from board.</p>
-          <p style={{color: "green"}}> {this.props.desc} </p>
-        </div>
-      } else {
-        message = <div><p style={{color:"red"}}>Times Up. Reload page to play again.</p> <p><b>Valid
-          words:</b> {this.props.valid_words.join(",")} </p></div>
-      }
+    }else if (this.props.timer) {
+      message = <div>
+        <p>Please find {this.props.valid_words.length} words from board.</p>
+        <p style={{color: "green"}}> {this.props.desc} </p>
+      </div>
+    }else{
+      message = <div><p style={{color:"red"}}>Times Up. Reload page to play again.</p> <p><b>Valid
+        words:</b> {this.props.valid_words.join(",")} </p></div>
     }
 
     return (
